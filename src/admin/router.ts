4969482a6ed3930a285ab6165dc2197f2593513b
longnguyen29103107 <long29103107@gmail.com>
2024-09-28 22:00:40 +0700

    update code

--- src/admin/router.ts
+++ src/admin/router.ts
@@ -1,22 +1,22 @@
-const Layout = () => import('@/admin/components/About.vue')
+const Layout = () => import('@/admin/AdminLayout.vue')
 const Policy = () => import('@/admin/components/Policy.vue')
 const About = () => import('@/admin/components/About.vue')
 
 export default [
   {
     path: '/admin',
-    component: Layout,
-    children: [
-      {
-        path: '',
-        name: 'AdminPolicy',
-        component: Policy
-      },
-      {
-        path: 'about',
-        name: 'AdminAbout',
-        component: About
-      }
-    ]
+    component: Layout
+    // children: [
+    //   {
+    //     path: '',
+    //     name: 'AdminPolicy',
+    //     component: Policy
+    //   },
+    //   {
+    //     path: 'about',
+    //     name: 'AdminAbout',
+    //     component: About
+    //   }
+    // ]
   }
 ]
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,11 @@ import router from '@/router'
 import Antd from 'ant-design-vue'
 import App from './App.vue'
 import { createApp } from 'vue'
+import 'ant-design-vue/dist/antd.css'
 
 const app = createApp(App)
 
 app.use(router)
 app.use(Antd)
-//registerLayouts(app)
 
 app.mount('#app')
