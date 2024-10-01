2bb637a2577b646241937682c4034180921ae600
long.nguyen <long.nguyen@siliconstack.com.au>
2024-10-01 17:35:32 +0700

    update code

--- src/common/components/Tab/index.vue
+++ src/common/components/Tab/index.vue
@@ -1,19 +1,24 @@
 <template>
-  <a-tab-pane :key="tabDataItem.key" :tab="tabDataItem.label">{{ tabDataItem.content }}</a-tab-pane>
+  <a-tabs :activeKey="activeKey">
+    <a-tab-pane v-for="item in tabData" :key="item.key" :tab="item.label">
+      {{ item.content }}
+    </a-tab-pane>
+  </a-tabs>
 </template>
 
 <script lang="ts" setup>
-import { type PropType, defineComponent, defineProps } from 'vue'
+import { type PropType, defineProps } from 'vue'
 import type TabType from '@/common/types/Tab/TabType'
 
 const props = defineProps({
-  tabDataItem: {
-    type: Object as PropType<TabType>,
+  tabData: {
+    type: Object as PropType<TabType[]>,
+    required: true,
+    default: []
+  },
+  activeKey: {
+    type: String,
     required: true
   }
 })
-
-defineComponent({
-
-}})
 </script>
